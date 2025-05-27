ecaaf2f9e9975fc2d76d420bfbc842bccc638a86
danielleit241 <danielleee241@gmail.com>
2025-05-27 13:05:37 +0700

    Add migration to update RoleName for RoleID 2
    This commit introduces a new migration class `UpdateSeedRoles` that modifies the `RoleName` for the role with `RoleID` 2 from "staff" to "nurse" in the `Up` method, and reverts it back to "staff" in the `Down` method. Additionally, the `Designer` file has been updated to reflect these changes in the model.

---
+++ SchoolMedicalServer.Infrastructure/Migrations/20250527055953_UpdateSeedRoles.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolMedicalServer.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSeedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Role",
+                keyColumn: "RoleID",
+                keyValue: 2,
+                column: "RoleName",
+                value: "nurse");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Role",
+                keyColumn: "RoleID",
+                keyValue: 2,
+                column: "RoleName",
+                value: "staff");
+        }
+    }
+}
