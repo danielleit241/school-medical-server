eb9325821c9cc9d9c8bff23757ce6dacc2ae12a3
danielleit241 <danielleee241@gmail.com>
2025-06-10 18:47:01 +0700

    Refactor medical inventory and user avatar request DTOs
    - Reintroduced `MedicalInventoryRequest` with full namespace and properties.
    - Replaced `UpdateAvatarUrlRequest` with `UserAvatarUrlRequest`.
    - Updated import statements in `MedicalInventoryController` and `UsersProfileController` for clarity.
    - Ensured `MedicalInventoryService` uses the new DTO namespace.

--- SchoolMedicalServer.Abstractions/Dtos/MedicalInventory/MedicalInventoryRequest.cs
+++ SchoolMedicalServer.Abstractions/Dtos/MedicalInventory/MedicalInventoryRequest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SchoolMedicalServer.Abstractions.Dtos
+namespace SchoolMedicalServer.Abstractions.Dtos.MedicalInventory
 {
     public class MedicalInventoryRequest
     {
